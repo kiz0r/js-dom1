d29636900453ee5b8c62db2aea7bef46b17d112e
Kirill Khomenko <kkhomenko10@gmail.com>
2023-01-06 21:35:51 +0100

    just last commit)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const [changeContentBtn, changeColorBtn] =
   document.querySelectorAll('button.btn');
 const colors = ['yellow', 'green', 'orange', 'purple', 'red', 'pink'];
+window.onload = firstImage;
 
 // 1) Після натискання кнопки змінити текст самої кнопки (див. textContent).
-
 changeContentBtn.addEventListener('click', () => {
   changeContentBtn.textContent = 'Happy New 2023 Year!';
   changeContentBtn.style.background = 'purple';
@@ -21,15 +21,15 @@ changeColorBtn.addEventListener('click', () => {
 // 3) Відобразити картинку. Зробити так, щоб при появі сторінки відображалася одна картинка, а при наведенні миші на неї відображалася інша.
 const bgImage = document.querySelector('.imageField');
 
-bgImage.addEventListener('mouseover', showImageWhenHover);
-bgImage.addEventListener('mouseleave', setImage);
+bgImage.onmouseover = secondImage;
+bgImage.onmouseleave = firstImage;
 
-function setImage() {
+function firstImage() {
   bgImage.style.backgroundImage =
     'url(https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_960_720.jpg)';
 }
 
-function showImageWhenHover() {
+function secondImage() {
   bgImage.style.backgroundImage =
     'url(https://cdn.pixabay.com/photo/2016/01/08/11/57/butterflies-1127666_960_720.jpg)';
 }
